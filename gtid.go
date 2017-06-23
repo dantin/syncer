5ada58d5a482434ce2794dd5d01e898d041fc53d
dantin <chengjie.ding@gmail.com>
2017-06-23 16:42:37 +0800

    implments Meta interface

--- gtid.go
+++ gtid.go
@@ -1,8 +1,20 @@
 package main
 
-import "github.com/siddontang/go-mysql/mysql"
+import (
+	"github.com/siddontang/go-mysql/mysql"
+	"github.com/juju/errors"
+)
 
 // GTIDSet wraps mysql.MysqlGTIDSet
 type GTIDSet struct {
 	*mysql.MysqlGTIDSet
+}
+
+func parseGTIDSet(gtidStr string) (GTIDSet, error) {
+	gs, err := mysql.ParseMysqlGTIDSet(gtidStr)
+	if err != nil {
+		return GTIDSet{}, errors.Trace(err)
+	}
+
+	return GTIDSet{gs.(*mysql.MysqlGTIDSet)}, nil
 }
--- meta.go
+++ meta.go
@@ -1,10 +1,21 @@
 package main
 
 import (
+	"bytes"
+	"os"
 	"sync"
 	"time"
 
+	"github.com/BurntSushi/toml"
+	"github.com/juju/errors"
 	"github.com/siddontang/go-mysql/mysql"
+	"github.com/siddontang/go/ioutil2"
+	"github.com/siddontang/go/log"
+	"fmt"
+)
+
+var (
+	maxSaveTime = 30 * time.Second
 )
 
 // Meta is the binlog meta information from sync source.
@@ -43,4 +54,88 @@ type LocalMeta struct {
 // NewLocalMeta creates a new LocalMeta.
 func NewLocalMeta(filename string) *LocalMeta {
 	return &LocalMeta{filename: filename, BinLogPos: 4}
+}
+
+// Load implements Meta.Load interface.
+func (lm *LocalMeta) Load() error {
+	file, err := os.Open(lm.filename)
+	if err != nil && !os.IsNotExist(errors.Cause(err)) {
+		return errors.Trace(err)
+	}
+	if os.IsNotExist(errors.Cause(err)) {
+		return nil
+	}
+	defer file.Close()
+
+	_, err = toml.DecodeReader(file, lm)
+	return errors.Trace(err)
+}
+
+// Save implements Meta.Save interface.
+func (lm *LocalMeta) Save(pos mysql.Position, gs GTIDSet, force bool) error {
+	lm.Lock()
+	defer lm.Unlock()
+
+	lm.BinLogName = pos.Name
+	lm.BinLogPos = pos.Pos
+	lm.BinLogGTID = gs.String()
+
+	if force {
+		return lm.Flush()
+	}
+	return nil
+}
+
+// Flush implements Meta.Flush interface.
+func (lm *LocalMeta) Flush() error {
+	var buf bytes.Buffer
+	e := toml.NewEncoder(&buf)
+	err := e.Encode(lm)
+	if err != nil {
+		log.Errorf("save meta info to file %s failed: %v", lm.filename, errors.ErrorStack(err))
+	}
+
+	err = ioutil2.WriteFileAtomic(lm.filename, buf.Bytes(), 0644)
+	if err != nil {
+		log.Errorf("save meta info to file %s failed: %v", lm.filename, errors.ErrorStack(err))
+	}
+
+	lm.saveTime = time.Now()
+	log.Infof("save position to file, binlog-name:%s binlog-pos:%d binlog-gtid:%v", lm.BinLogName, lm.BinLogPos, lm.BinLogGTID)
+
+	return nil
+}
+
+// Check implements Meta.GTID interface
+func (lm *LocalMeta) Check() bool {
+	lm.RLock()
+	defer lm.RUnlock()
+
+	if time.Since(lm.saveTime) >= maxSaveTime {
+		return true
+	}
+
+	return false
+}
+
+// Pos implements Meta.Pos interface.
+func (lm *LocalMeta) Pos() mysql.Position {
+	lm.RLock()
+	defer lm.RUnlock()
+
+	return mysql.Position{Name: lm.BinLogName, Pos: lm.BinLogPos}
+}
+
+// GTID implements Meta.GTID interface.
+func (lm *LocalMeta) GTID() (GTIDSet, error) {
+	lm.RLock()
+	defer lm.RUnlock()
+
+	return parseGTIDSet(lm.BinLogGTID)
+}
+
+func (lm *LocalMeta) String() string {
+	pos := lm.Pos()
+	gs, _ := lm.GTID()
+	return fmt.Sprintf("syncer-binlog = %v, syncer-binlog-gtid = %v", pos, gs)
 }
