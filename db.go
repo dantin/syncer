d63662d8b5bc40ea9b5d69831d197df18a15e6e7
dantin <chengjie.ding@gmail.com>
2017-06-23 10:09:30 +0800

    add close db functions

--- db.go
+++ db.go
@@ -1,3 +1,28 @@
 package main
 
+import (
+	"database/sql"
+
+	"github.com/juju/errors"
+	"github.com/ngaut/log"
+)
+
 var safeMode bool
+
+func closeDBs(dbs ...*sql.DB) {
+
+	for _, db := range dbs {
+		err := closeDB(db)
+		if err != nil {
+			log.Errorf("close db failed: %v", err)
+		}
+	}
+}
+
+func closeDB(db *sql.DB) error {
+	if db == nil {
+		return nil
+	}
+
+	return errors.Trace(db.Close())
+}
