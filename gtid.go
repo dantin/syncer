6627aa04cd8323b03b0f0af49c0987eb2b8c3aaa
dantin <chengjie.ding@gmail.com>
2017-06-23 17:02:09 +0800

    add gtid support functions

--- gtid.go
+++ gtid.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"github.com/siddontang/go-mysql/mysql"
 	"github.com/juju/errors"
+	"github.com/siddontang/go-mysql/mysql"
 )
 
 // GTIDSet wraps mysql.MysqlGTIDSet
@@ -17,4 +17,21 @@ func parseGTIDSet(gtidStr string) (GTIDSet, error) {
 	}
 
 	return GTIDSet{gs.(*mysql.MysqlGTIDSet)}, nil
+}
+
+func (g GTIDSet) delete(uuid string) {
+	delete(g.Sets, uuid)
+}
+
+func (g GTIDSet) contain(uuid string) bool {
+	_, ok := g.Sets[uuid]
+	return ok
+}
+
+func (g GTIDSet) get(uuid string) *mysql.UUIDSet {
+	return g.Sets[uuid]
+}
+
+func (g GTIDSet) all() map[string]*mysql.UUIDSet {
+	return g.Sets
 }
